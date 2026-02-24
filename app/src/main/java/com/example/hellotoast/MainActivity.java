e50df043427644a80f3ff837d87dce6f44f1cb97
sarrah yousfi <sarrahyousfi18@gmail.com>
2026-02-23 17:45:37 -1100

    Premier commit: Application Toast Android

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hellotoast;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    // Toast
+    public void showToast(View view) {
+        Toast.makeText(this, "Button Toast Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    // Compteur
+    public void countUp(View view) {
+        mCount++;
+
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
